e51a5eda8a44b3bcc8b9b5a5819af957fa6a8f1d
Tushar2023 <tusharsingla078@gmail.com>
2022-10-20 22:47:45 +0530

    added collect_stats for cpu_monitoring

--- Monitoring/cpu_monitoring.py
+++ Monitoring/cpu_monitoring.py
@@ -1,40 +1,29 @@
-from time import sleep
+from stubs import get_vm_pid
 from monitor import Monitor
 import psutil
-from threading import Thread
-import config
+from typing import List, Any, Tuple, Dict, int
+
 
 
 class CpuMonitor(Monitor):
     def __init__(self, vm_ids=...) -> None:
         super().__init__(vm_ids)
+        psutil.cpu_percent(interval=None, percpu=True)
+        for vm_id in self.vm_ids:
+            vm_pid = get_vm_pid(vm_id)
+            psutil.Process(vm_pid).cpu_percent(interval=None)/psutil.cpu_count()
+    
+    def collect_stats(self) -> Tuple(float, Dict[str, float]):
+        host_per_cpu_usage = psutil.cpu_percent(interval=None, percpu=True)
+        avg_host_cpu_usage = 0
+        for cpu in host_per_cpu_usage:
+            avg_host_cpu_usage += cpu
+        avg_host_cpu_usage /= len(host_per_cpu_usage)
 
-        th = Thread(target=update, args=())
-        th.start()
+        vm_stats = {}
+        for vm_id in self.vm_ids:
+            vm_pid = get_vm_pid(vm_id)
+            vm_stats[vm_id] = psutil.Process(vm_pid).cpu_percent(interval=None)/psutil.cpu_count()
+        
+        return (avg_host_cpu_usage, vm_stats)
 
-    def update(self):
-        #get cpu usage (percentage) of host using psutil
-        total_intervals = 0
-        while True:
-            total_intervals = total_intervals + 1
-            host_per_cpu_usage = psutil.cpu_percent(interval=None, percpu=True)
-            avg_host_cpu_usage = 0
-            for cpu in host_per_cpu_usage:
-                avg_host_cpu_usage += cpu
-            avg_host_cpu_usage /= len(host_per_cpu_usage)
-
-            # update histogram and timeseries for host
-            self.update_histogram(-1, avg_host_cpu_usage, host=True)
-
-            # if total_intervals > config.TIME_SERIES_LEN:
-            #     self.host_timeseries.pop(0) 
-            #     self.host_timeseries.append(avg_host_cpu_usage)
-            # else:
-            #     self.host_timeseries[total_intervals-1] = avg_host_cpu_usage
-
-
-            sleep(config.MONITOR_INTERVAL)
-
-
-    def get_host_stats() -> Tuple(List[Any], Dict[int : int]):
-        pass
