5883a5e73cec7c6b3dfa1087ad76744fd4c84210
Ramneek Singh <74413910+RamneekSingh24@users.noreply.github.com>
2022-10-20 16:04:33 +0530

    Ramneek/interfaces (#1)
    * Create Interfaces for monitoring
    
    * Create Interfaces for load balancing

---
+++ Monitoring/stubs.py
@@ -0,0 +1,8 @@
+def get_vm_pid(vm_id):
+    pass
+
+def get_vm_tap_device(vm_id):
+    pass
+
+# Add other things needed, this will be replaced later, possibly with database calls as we get info. from other teams
+
