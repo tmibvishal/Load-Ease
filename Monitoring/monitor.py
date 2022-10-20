998609eeed263f7486c5bf3e88c1b06a7a19208f
Tushar2023 <tusharsingla078@gmail.com>
2022-10-20 17:01:35 +0530

    initial commit update histogram added

--- Monitoring/monitor.py
+++ Monitoring/monitor.py
@@ -4,7 +4,7 @@ import config
 class Monitor:
     def __init__(self, vm_ids=[]) -> None:
         # Histogram[i] = percentage number of times usage was in [i, i + 5)
-        self.host_histogram : dict[int : int] = {i : 0 for i in range(0, 5, 100)}
+        self.host_histogram : dict[int : float] = {i : 0 for i in range(0, 100, 5)}
         self.host_timeseries = [0] * config.TIME_SERIES_LEN
 
         self.vm_histograms = { vm_id : {i : 0 for i in range(0, 5, 100)} for vm_id in vm_ids }
@@ -23,4 +23,21 @@ class Monitor:
         
     # Vm moved to a new host, need to refresh the data(vm pid etc..) for the VM 
     def vm_moved(vm_id) -> None:
+        pass
+
+    def update_histogram(self, vm_id, resource_usage, host: bool = False) -> None:
+        if host:
+            for interval in range(0,100,5):
+                if resource_usage >= interval and resource_usage < interval + 5:
+                    self.host_histogram[interval] = (self.host_histogram[interval]*(total_intervals-1) + 1)/total_intervals
+                else:
+                    self.host_histogram[interval] = (self.host_histogram[interval]*(total_intervals-1))/total_intervals
+        else:
+            for interval in range(0,100,5):
+                if resource_usage >= interval and resource_usage < interval + 5:
+                    self.vm_histograms[vm_id][interval] = (self.vm_histograms[vm_id][interval]*(total_intervals-1) + 1)/total_intervals
+                else:
+                    self.vm_histograms[vm_id][interval] = (self.vm_histograms[vm_id][interval]*(total_intervals-1))/total_intervals
+
+    def update_timeseries(self, vm_id, resource_usage, host: bool = False) -> None:
         pass
