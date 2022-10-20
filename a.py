2c5477e0d2d7b371c2e94c1f01c21a2143cc95d7
Rishi Shah <rishi.shah10122001@gmail.com>
2022-10-20 18:45:42 +0530

    Small Update

--- a.py
+++
@@ -1,8 +0,0 @@
-# abc='requirements'
-# f = open("Monitoring/{}.txt".format(abc), "r")
-# print(f.read())
-from typing import List, Any, Tuple, Dict
-def f() -> Tuple(int,int):
-    return (1,2)
-
-print(f())
