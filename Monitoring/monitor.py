40e7dd2132e5ea3d97a09b07b14ee94707fa9d0a
Vishal Singh <31121102+tmibvishal@users.noreply.github.com>
2022-10-20 18:32:21 +0530

    Memory Management basic psutils functions (#3)
    * add get_vm_infos
    
    * add _get_all_vm_stats in MemoryMonitor
    
    * setup memory monitor
    
    * improve merge

--- Monitoring/monitor.py
+++ Monitoring/monitor.py
@@ -8,7 +8,7 @@ class Monitor:
     def __init__(self, vm_ids=[]) -> None:
         # Histogram[i] = percentage number of times usage was in [i, i + 5)
         self.vm_ids = vm_ids
-        self.host_histogram : dict[int : int] = {i : 0 for i in range(0, 100, 5)}
+        self.host_histogram : dict[int, float] = {i : 0 for i in range(0, 100, 5)}
         self.host_timeseries = [0] * config.TIME_SERIES_LEN
 
         self.vm_histograms = { vm_id : {i : 0 for i in range(0, 100, 5)} for vm_id in vm_ids }
@@ -30,13 +30,13 @@ class Monitor:
 
     # Inheriting classes implement
     # Implement in base class, add a new vm for monitoring
-    def register_vm(self, vm_id) -> None:
+    def register_vm(self, vm_id: str) -> None:
         pass
 
     # Inheriting classes implement
     # Vm moved to a new host, can remove this vm from monitoring
     # Clean up the datastructures for this vm
-    def vm_moved(self, vm_id) -> None:
+    def vm_moved(self, vm_id: str) -> None:
         pass
         
 
@@ -55,7 +55,7 @@ class Monitor:
             time.sleep(config.MONITOR_INTERVAL)
 
 
-    def update_histogram(self, vm_id, resource_usage, host: bool = False) -> None:
+    def update_histogram(self, vm_id: str, resource_usage, host: bool = False) -> None:
         hist = self.host_histogram
         if not host:
             hist = self.vm_histograms[vm_id]
@@ -67,7 +67,7 @@ class Monitor:
                 hist[interval] = (hist[interval] * (self.total_intervals - 1)) / self.total_intervals
       
 
-    def update_timeseries(self, vm_id, resource_usage, host: bool = False) -> None:
+    def update_timeseries(self, vm_id: str, resource_usage, host: bool = False) -> None:
         timeseries = self.host_timeseries
         if not host:
             timeseries = self.vm_timeseries[vm_id]
@@ -79,6 +79,6 @@ class Monitor:
     def get_host_stats(self) -> Tuple(List[float], Dict[int : float]):
         return self.host_timeseries, self.host_histogram
 
-    def get_vm_stats(self, vm_id) -> Tuple(List[float], Dict[int : float]):
+    def get_vm_stats(self, vm_id: str) -> Tuple(List[float], Dict[int : float]):
         return self.vm_timeseries[vm_id], self.vm_histograms[vm_id]
 
--- Monitoring/network_monitoring.py
+++ Monitoring/network_monitoring.py
@@ -1,4 +1,6 @@
+from typing import Any, Dict, List, Tuple, Union
 from monitor import Monitor
+import psutil
 
 class NetworkMonitor(Monitor):
     pass
