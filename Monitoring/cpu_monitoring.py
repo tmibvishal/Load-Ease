c1b26fe80a669bea52e23586b0f9fe3df9459409
Tushar Singla <tusharsingla078@gmail.com>
2022-10-21 11:30:04 +0530

    Collect Stats for Cpu Monitoring (#5)
    * initial commit update histogram added
    
    * added collect_stats for cpu_monitoring
    
    * Cleaning

--- Monitoring/cpu_monitoring.py
+++ Monitoring/cpu_monitoring.py
@@ -1,40 +1,48 @@
-from time import sleep
+from stubs import get_vm_pid
 from monitor import Monitor
 import psutil
-from threading import Thread
-import config
+from typing import List, Any, Tuple, Dict, int
+
 
 
 class CpuMonitor(Monitor):
     def __init__(self, vm_ids=...) -> None:
         super().__init__(vm_ids)
+        # psutil cpu_percent gives garbage value on the first run
+        # so making an initial call to get rid of it
+        psutil.cpu_percent(interval=None, percpu=True)
+        for vm_id in self.vm_ids:
+            vm_pid = get_vm_pid(vm_id)
+            psutil.Process(vm_pid).cpu_percent(interval=None)/psutil.cpu_count()
+    
+    def collect_stats(self) -> Tuple(float, Dict[str, float]):
+        '''
+        Returns the average cpu usage of the Host and all the VMs
+        '''
+        # interval=None means that the cpu usage is calculated
+        # since the last call to cpu_percent
+        # it is a non-blocking call
 
-        th = Thread(target=update, args=())
-        th.start()
+        # percpu=True means that the cpu usage is calculated
+        # for each core
+        host_per_cpu_usage = psutil.cpu_percent(interval=None, percpu=True)
+        avg_host_cpu_usage = 0
+        for cpu in host_per_cpu_usage:
+            avg_host_cpu_usage += cpu
+        # cpu_percent returns a list of cpu usage for each core
+        # so we need to divide by the number of cores
+        # to get the average cpu usage
+        avg_host_cpu_usage /= len(host_per_cpu_usage)
 
-    def update(self):
-        #get cpu usage (percentage) of host using psutil
-        total_intervals = 0
-        while True:
-            total_intervals = total_intervals + 1
-            host_per_cpu_usage = psutil.cpu_percent(interval=None, percpu=True)
-            avg_host_cpu_usage = 0
-            for cpu in host_per_cpu_usage:
-                avg_host_cpu_usage += cpu
-            avg_host_cpu_usage /= len(host_per_cpu_usage)
+        # vm_id to cpu usage
+        vm_stats = {}
+        for vm_id in self.vm_ids:
+            # get the pid of the vm
+            vm_pid = get_vm_pid(vm_id)
+            # divide by the number of cores to get the average cpu usage
+            # because cpu_percent might return a value > 100
+            # as it sums up the cpu usage of all the cores
+            vm_stats[vm_id] = psutil.Process(vm_pid).cpu_percent(interval=None)/psutil.cpu_count()
+        
+        return (avg_host_cpu_usage, vm_stats)
 
-            # update histogram and timeseries for host
-            self.update_histogram(-1, avg_host_cpu_usage, host=True)
-
-            # if total_intervals > config.TIME_SERIES_LEN:
-            #     self.host_timeseries.pop(0) 
-            #     self.host_timeseries.append(avg_host_cpu_usage)
-            # else:
-            #     self.host_timeseries[total_intervals-1] = avg_host_cpu_usage
-
-
-            sleep(config.MONITOR_INTERVAL)
-
-
-    def get_host_stats() -> Tuple(List[Any], Dict[int : int]):
-        pass
