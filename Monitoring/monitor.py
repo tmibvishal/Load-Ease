956cccbb1a57bac087c7807af7e433e714b8b92e
Ramneek Singh <74413910+RamneekSingh24@users.noreply.github.com>
2022-10-23 14:24:23 +0530

    Ramneek/loadbalancing (#7)
    * Create Interfaces for monitoring
    
    * Create Interfaces for load balancing
    
    * update interfaces
    
    * add basic code for provisioning
    
    * Update monitor.py
    
    * fix balancer.py

--- Monitoring/monitor.py
+++ Monitoring/monitor.py
@@ -7,11 +7,11 @@ class Monitor:
     def __init__(self, vm_ids : List[Any]=[]) -> None:
         # Histogram[i] = percentage number of times usage was in [i, i + 5)
         self.vm_ids = vm_ids
-        self.host_histogram : dict[int, float] = {i : 0 for i in range(0, 100, 5)}
-        self.host_timeseries = [0] * config.TIME_SERIES_LEN
+        self.host_histogram : dict[int : float] = {i : 0 for i in range(0, 100, 5)}
+        self.host_timeseries = []
 
         self.vm_histograms = { vm_id : {i : 0 for i in range(0, 100, 5)} for vm_id in vm_ids }
-        self.vm_timeseries = { vm_id : [0] * config.TIME_SERIES_LEN for vm_id in vm_ids } 
+        self.vm_timeseries = { vm_id : [] for vm_id in vm_ids }
 
         self.total_intervals = 0
 
@@ -23,25 +23,27 @@ class Monitor:
     
     # Inheriting classes implement
     # return (host_usage %, and Dict[vmid : vm usage %])
+    # Flaot = Percentage, Ex: 95.2 % -> 95.2 (not 0.952)
     def collect_stats(self) -> Tuple[float, Dict[str, float]]:
         pass
 
 
     # Inheriting classes implement
     # Implement in base class, add a new vm for monitoring
-    def register_vm(self, vm_id: str) -> None:
+    def register_vm(self, vm_id) -> None:
         pass
 
     # Inheriting classes implement
     # Vm moved to a new host, can remove this vm from monitoring
     # Clean up the datastructures for this vm
-    def vm_moved(self, vm_id: str) -> None:
+    def vm_moved(self, vm_id) -> None:
         pass
         
 
     def update(self) -> None:
         while True:
             self.total_intervals += 1
+
             (host_stat, vm_stats) = self.collect_stats()
 
             for vm_id, usage in vm_stats.items():
@@ -54,7 +56,7 @@ class Monitor:
             time.sleep(config.MONITOR_INTERVAL)
 
 
-    def update_histogram(self, vm_id: str, resource_usage, host: bool = False) -> None:
+    def update_histogram(self, vm_id, resource_usage, host: bool = False) -> None:
         hist = self.host_histogram
         if not host:
             hist = self.vm_histograms[vm_id]
@@ -66,13 +68,13 @@ class Monitor:
                 hist[interval] = (hist[interval] * (self.total_intervals - 1)) / self.total_intervals
       
 
-    def update_timeseries(self, vm_id: str, resource_usage, host: bool = False) -> None:
-        timeseries = self.host_timeseries
+    def update_timeseries(self, vm_id, resource_usage, host: bool = False) -> None:
+        timeseries : List = self.host_timeseries
         if not host:
             timeseries = self.vm_timeseries[vm_id]
-        idx = (self.total_intervals - 1) % config.TIME_SERIES_LEN
-
-        timeseries[idx] = resource_usage
+        timeseries.append(resource_usage)
+        if len(timeseries) > config.TIME_SERIES_LEN:
+            timeseries.pop(0)
 
             
     def get_host_stats(self) -> Tuple[List[float], Dict[int, float]]:
