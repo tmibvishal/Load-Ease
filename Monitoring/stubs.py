d4da66f44332e0f3d787d734e617b4ca5c85e0f2
Ramneek Singh <74413910+RamneekSingh24@users.noreply.github.com>
2022-10-29 17:40:42 +0530

    populate redis (#30)
    * populate redis

--- Monitoring/stubs.py
+++ Monitoring/stubs.py
@@ -3,23 +3,17 @@ import redis
 from config import rds, HOST_ID
 
 def get_vm_ids():
-    # TODO (ramneek): Get this from redis
-    # vm_ids = rds.smembers(f"vms_in_host:{HOST_ID}")
-    # return vm_ids
-    return ['vm1']
+    vm_ids = rds.smembers(f"vms_in_host:{HOST_ID}")
+    return [vm_id.decode('utf-8') for vm_id in vm_ids]
 
 
 def get_vm_pid(vm_id: str) -> int:
-    # TODO (ramneek): Get this from redis
-    # return rds.hget(f"vm_configs:{vm_id}", 'pid')
-    return os.getpid()
+    return int(rds.hget(f"vm_configs:{vm_id}", 'pid'))
 
 def get_vm_tap_device(vm_id: str) -> str:
-    # TODO (ramneek): Get this from redis
-    # return rds.hget(f"vm_configs:{vm_id}", 'tap_device')
-    return 'eth0'
+    return rds.hget(f"vm_configs:{vm_id}", 'tap_device').decode('utf-8')
 
 def get_host_tap_device():
-    pass
+    return rds.hget(f"host_configs:{HOST_ID}", 'tap_device').decode('utf-8')
 
 # Add other things needed, this will be replaced later, possibly with database calls as we get info. from other teams
